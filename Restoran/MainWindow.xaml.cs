366baa2204a96908f49bdc247bbea36d2b4af484
Vlad-true-proger <vladeus007@gmail.com>
2015-06-01 23:14:56 +0300

    Если это появится, то хорошо

--- Restoran/MainWindow.xaml.cs
+++ Restoran/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,50 @@ namespace Restoran
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> povars = new List<string>();
+        List<string> names = new List<string>();
+        List<string> dishes = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            CreateDataBase();
+        }
+
+        private void CreateDataBase()
+        {
+            //Новые блюда
+            for(int i=0; i<15; i++)
+            {
+
+            }
+            //Новые повара
+            //Новые заказы
+            //Новая карта
+            //Новый счет
+        }
+
+
+
+        public List<string> ReadFromFile(string path)
+        {
+            List<string> mass = new List<string>();
+            Stream s = new FileStream(path, FileMode.Open);
+            using (StreamReader sr = new StreamReader(s, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    mass.Add(line);
+                }
+            } return mass;
+        }
+
+        public int Random(int min, int max)
+        {
+            Random r = new Random(int.Parse(DateTime.Now.Millisecond.ToString()));
+           int t= r.Next(min, max);
+           return t;
         }
     }
 }
